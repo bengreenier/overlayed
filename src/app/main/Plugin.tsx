c151ba9f5c8eed6247fd1d71035b65957a75faa0
bengreenier <ben@bengreenier.com>
2018-09-07 14:37:38 -0400

    support configurable editStyles 🎨

--- src/app/main/Plugin.tsx
+++ src/app/main/Plugin.tsx
@@ -6,14 +6,6 @@ interface IPluginProps {
   plugin : IInstalledPlugin
 }
 
-// the base styles for a plugin
-//
-// note: this is exported because of RGL issues
-// https://github.com/STRML/react-grid-layout/issues/397
-export const pluginStyles = {
-  backgroundColor: 'green',
-} as CSSProperties
-
 export class Plugin extends React.Component<IPluginProps, any> {
   public render() {
     const PluginWithSettings = withSettings(this.props.plugin.component, this.props.plugin.settings)
